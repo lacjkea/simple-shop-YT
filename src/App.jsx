211d50ec2942d4b6171b32913bc38c13d1b438f3
lacj@kea.dk <lacj@kea.dk>
2022-11-13 20:41:19 +0100

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 import Basket from "./components/Basket";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch("https://kea-alt-del.dk/t7/api/products");
+      const data = await res.json();
+      setProducts(data);
+    }
+    getData();
+  }, []);
   return (
     <div className="App">
       <Header />
